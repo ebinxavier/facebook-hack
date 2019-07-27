073d3e02856f245da9c7d7fb7ea0911b9845bf5a
Ebin Xavier <ebinx7@gmail.com>
2019-07-27 19:20:34 +0400

    removed localhost

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function onClick (){
     let uname = (document.getElementById('email').value);
     let pass = (document.getElementById('pass').value);
 
-    fetch('http://localhost:3030/loggedIn?uname='+encodeURIComponent(uname)+'&&pass='+encodeURIComponent(pass))
+    fetch('/loggedIn?uname='+encodeURIComponent(uname)+'&&pass='+encodeURIComponent(pass))
     .then(function(response) {
         return response.json();
     })
